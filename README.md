058e0b8c01dbd22797279a23130a8cca908c96ff
Songwong Tasneeyapant <nat@nats-MacBook-Pro.local>
2017-05-15 02:10:27 -0500

    #1

--- README.md
+++ README.md
@@ -4,5 +4,5 @@ From: Bo Qian <br/>
 https://www.youtube.com/playlist?list=PL5jc9xFGsL8E12so1wlMS0r0hTQoJL74M
 
 Concurrent Programming <br/>
-1 - Multiprocessing - can use many computers at the same time
+1 - Multiprocessing - can use many computers at the same time <br/>
 2 - Multithreading  - use shared memory, so cannot use more than one computer
---
+++ tutorial_1.cpp
@@ -0,0 +1,35 @@
+/* Songwong Tasneeyapant
+ * C++ Threading #1: Intoduction
+*/
+
+#include <iostream>
+#include <thread>
+using namespace std;
+
+void function_1() {
+    std::cout << "Beauty is only skin-deep" << std::endl;
+}
+
+int main() {
+    std::thread t1(function_1); // t1 starts running
+    //t1.join();  // main thread waits for t1 to finish
+
+    t1.detach();// t1 will run freely on its own -- daemon process
+                // Since the main thread is no longer connected
+                // C++ runtime library is responsible for reclaiming resources of t1
+
+                // In this case, the main thread will finish before t1 even
+                // has a chance to deliver a message to stdout
+
+                // If two threads are sharing certain resource (in this case "stdout")
+                // the thread that owns that resource (in this case "main thread")
+                // should have live as long as the other thread is accessing the resource
+
+    // You can join()/detach() only once
+    // once detach, forever detach (cannot join after that)
+    if(t1.joinable()){
+        t1.join();  // still cannot join, but the program will not crash
+    }
+
+    return 0;
+}
